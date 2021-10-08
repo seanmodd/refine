fd53e52e7369867f65a0d4b1aa1db66781d1ad08
Sean <>
2021-10-07 23:13:50 -0700

    Fixed it it finally works

--- examples/fineFoods/webpack.config.js
+++ examples/fineFoods/webpack.config.js
@@ -1,45 +1,45 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+import nodeExternals from "webpack-node-externals";
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
+    entry: "./src/app.jsx",
 
-  entry: './src/app.jsx',
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
 
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
+    devtool: "eval-source-map",
 
-  devtool: 'eval-source-map',
-
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        include: [path.resolve(__dirname, "src")],
-        use: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader','css-loader']
-      },
-      {
-        test: /\.less$/,
-        use: ['style-loader','css-loader',"less-loader"]
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192
-            }
-          }
-        ]
-      }
-    ]
-  }
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                include: [path.resolve(__dirname, "src")],
+                use: "babel-loader",
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.less$/,
+                use: ["style-loader", "css-loader", "less-loader"],
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            limit: 8192,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
 };
